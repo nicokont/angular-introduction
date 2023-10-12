ca12ed120ddaa1b6f57672a7bdfdde5f1b9f9c6c
nicokont <nicokont@gmail.com>
2023-10-12 20:29:58 +0300

    new person component

---
+++ src/app/person/person.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-person',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent {
+  person = {
+    givenName: 'Nikolas',
+    surName: 'Kontaxakis',
+    age: 24,
+    email: '@aueb.gr'
+  };
+}
