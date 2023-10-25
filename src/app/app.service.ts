04792bce1cb4ec7e9f349f04235328c047881e20
nicokont <nicokont@gmail.com>
2023-10-25 13:49:39 +0300

    update user

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Person } from './interfaces/person';
 
+const API = 'http://localhost:3000/users'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,18 +11,22 @@ export class AppService {
   constructor(private http: HttpClient = inject(HttpClient)) {}
 
   getAllUsers() {
-    return this.http.get<Person[]>('http://localhost:3000/users');
+    return this.http.get<Person[]>(API);
   }
 
   getUserById(id:number) {
-    return this.http.get<Person>(`http://localhost:3000/users/${id}`)
+    return this.http.get<Person>(`${API}/${id}`)
   }
 
   addUser(user: Person) {
-    return this.http.post<Person>('http://localhost:3000/users', user)
+    return this.http.post<Person>(API, user)
   }
 
   deleteUser(id: number) {
-    return this.http.delete<Person>(`http://localhost:3000/users/${id}`)
+    return this.http.delete<Person>(`${API}/${id}`)
+  }
+
+  updateUser(user: Person) {
+    return this.http.put<Person>(`${API}/${user.id}`, user)
   }
 }
